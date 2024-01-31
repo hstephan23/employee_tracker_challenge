00c2c052ffdd24469eb7f253f2c21d4818ea5845
hstephan23 <harrison@stephanclan.com>
2024-01-30 22:59:01 -0700

    added more logic to the formatting

--- public/questions.js
+++ public/questions.js
@@ -1,53 +1,100 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 class Question {
     constructor (option) {
         this.option = option;
-        this.db = mysql.createConnection(
-            {
+        this.db = mysql.createPool({
                 host: 'localhost',
                 user: 'root',
-                password: '',
+                password: 'kajTun-wykse4-myrmiv',
                 database: 'employee_db'
-            }
-        );
-    };
-
-    static async updateEmployee() {
-        const sql = `SELECT department.id AS id, department.name AS name, role.title AS title FROM department JOIN role ON department.id = role.department_id`;
-        return new Promise((resolve, reject) => {
-            this.db.query(sql, (err, results) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    resolve(results);
-                }
-            });
         });
     };
 
-    static async viewRoles () {
-        console.log('View role');
+    async updateEmployee() {
+        const sql = `SELECT employee.id AS id, employee.first_name AS first_name, role.title AS title, department.name AS department, role.salary AS salary, employee.manager_id AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;`;
+        try {
+            const [results] = await this.db.query(sql);
+            const formattedData = results.map(result => ({
+                id: result.id,
+                first_name: result.first_name,
+                title: result.title,
+                department: result.department,
+                salary: result.salary,
+                manager: result.manager
+            }));
+            console.table(formattedData);
+            return results;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    };
+
+    async viewRoles () {
+        const sql = `SELECT role.id AS id, role.title AS title, department.name AS department, role.salary AS salary FROM role JOIN department on role.department_id = department.id ORDER BY role.id;`;
+        try {
+            const [results] = await this.db.query(sql);
+            const formattedData = results.map(result => ({
+                id: result.id,
+                title: result.title,
+                department: result.department,
+                salary: result.salary,
+            }));
+            console.table(formattedData);
+            return results;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     };
     
-    static async addRole() {
+    async addRole() {
         console.log('Add role');
     };
 
-    static async viewDepartments() {
-        console.log('View departments');
+    async viewDepartments() {
+        const sql = `SELECT department.id AS id, department.name AS name FROM department ORDER BY department.id;`;
+        try {
+            const [results] = await this.db.query(sql);
+            const formattedData = results.map(result => ({
+                id: result.id,
+                name: result.name
+            }));
+            console.table(formattedData);
+            return results;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     };
 
-    static async addDepartment() {
+    async addDepartment() {
         console.log('Add department');
     };
 
-    static async viewEmployees() {
-        console.log('View employees');
+    async viewEmployees() {
+        const sql = `SELECT employee.id AS id, employee.first_name AS first_name, role.title AS title, department.name AS department, role.salary AS salary, employee.manager_id AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;`;
+        try {
+            const [results] = await this.db.query(sql);
+            const formattedData = results.map(result => ({
+                id: result.id,
+                first_name: result.first_name,
+                title: result.title,
+                department: result.department,
+                salary: result.salary,
+                manager: result.manager
+            }));
+            console.table(formattedData);
+            return results;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     };
 
-    static async quit() {
+    async quit() {
         console.log('Quit');
     };
 
