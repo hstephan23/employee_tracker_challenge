da1bca222688bd73fcaa9f38c869180de8b446d8
hstephan23 <harrison@stephanclan.com>
2024-01-30 23:28:55 -0700

    adjusted the seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
-INSERT INTO department (id, name) 
-VALUES (001, 'Engineering'),
-    (002, 'Web Development'),
-    (003, 'Administration');
+INSERT INTO department (name) 
+VALUES ('Engineering'),
+    ('Web Development'),
+    ('Administration');
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (001, 'Engineer', 99999.00, 001),
-    (002, 'Administrator Assistant', 49999.00, 003),
-    (003, 'Junior Web Developer', 74999.00, 002),
-    (004, 'Senior Web Developer', 89999.00, 002),
-    (005, 'CEO', 149999.00, 003);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Engineer', 99999.00, 001),
+    ('Administrator Assistant', 49999.00, 003),
+    ('Junior Web Developer', 74999.00, 002),
+    ('Senior Web Developer', 89999.00, 002),
+    ('CEO', 149999.00, 003);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (001, 'Harrison', 'Stephan', 003, 213),
