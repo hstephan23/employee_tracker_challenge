d8ec8b3c8fa543dc836e05cb59a862e5e335d14a
hstephan23 <harrison@stephanclan.com>
2024-01-30 23:27:48 -0700

    updated the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-    id INT NOT NULL, 
+    id INT NOT NULL AUTO_INCREMENT, 
     title VARCHAR(30) NOT NULL,
     salary DEC NOT NULL,
     department_id INT,
