de1a22f7b96ecbef9edb9549a01778e2c792f16a
hstephan23 <harrison@stephanclan.com>
2024-01-30 17:44:05 -0700

    added the base of things

--- public/questions.js
+++ public/questions.js
@@ -1,2 +1,18 @@
 const inquirer = require('inquirer');
 
+function prompts () {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'options',
+                message: 'What would you like to do?',
+                options: ['Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'View All employees', 'Quit']
+            }
+        ])
+        .then(
+            console.log(options)
+        )
+}
+
+prompts();
