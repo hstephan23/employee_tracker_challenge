9af473d432fdf1c25b77aca3825dac4f847d2b35
hstephan23 <harrison@stephanclan.com>
2024-01-30 22:59:28 -0700

    adding this file to it

--- db/query.sql
+++ db/query.sql
@@ -7,8 +7,33 @@ FROM role;
 SELECT * 
 FROM employee;
 
+-- viewEmployees
+SELECT 
+    employee.id AS id, 
+    employee.first_name AS first_name,
+    role.title AS title,
+    department.name AS department,
+    role.salary AS salary,
+    employee.manager_id AS manager
+FROM employee
+JOIN role ON employee.role_id = role.id
+JOIN department ON role.department_id = department.id
+ORDER BY employee.id;
 
-SELECT department.id AS id, department.name AS name, role.title AS title
-FROM department
-JOIN role ON department.id = role.department_id;
+-- viewDepartments
+SELECT 
+    department.id AS id,
+    department.name AS name
+FROM department;
+ORDER BY department.id
+
+-- viewRoles
+SELECT 
+    role.id AS id, 
+    role.title AS title,
+    department.name AS department,
+    role.salary AS salary
+FROM role
+JOIN department on role.department_id = department.id
+ORDER BY role.id;
 
